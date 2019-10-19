a87bd133e6582ca5c459bba738736aade3d985b2
Ubuntu <ubuntu@ip-172-31-38-9.us-east-2.compute.internal>
2019-10-19 00:17:53 +0000

    done

--- opencv_web.sh
+++ opencv_web.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "starting"
+
+yes|sudo apt-get install software-properties-common
+yes|sudo apt-add-repository universe
+yes|sudo apt-get update
+yes|sudo apt-get install python-pip
+
+yes|sudo apt install python
+yes|sudo apt install python-pip
+yes|pip install numpy scipy matplotlib scikit-learn jupyter
+yes|pip install opencv-contrib-python
+yes|sudo apt-get install python3-pip
+python3 -m pip install jupyterhub
+jupyterhub --generate-config
+
+echo "install in sudo to get port 80"
