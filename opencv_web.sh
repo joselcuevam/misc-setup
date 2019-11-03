fc5b9be6b568768658a14b0e988aaa24532fb96f
jose <joseluis7012@gmail.com>
2019-11-03 14:50:19 +0000

    done

--- opencv_web.sh
+++ opencv_web.sh
@@ -12,15 +12,16 @@ yes|sudo apt-add-repository universe
 yes|sudo apt-get update
 yes|sudo apt-get install python-pip
 
-yes|sudo apt install python
-yes|sudo apt install python-pip
-yes|pip install numpy scipy matplotlib scikit-learn jupyter
+#yes|sudo apt install python
+#yes|sudo apt install python-pip
+yes|sudo pip install numpy scipy matplotlib scikit-learn jupyter  --no-cache-dir
 yes|sudo apt-get install python-numpy python-scipy python-matplotlib 
-yes|pip install opencv-contrib-python
+yes|sudo pip install opencv-contrib-python --no-cache-dir
 yes|sudo apt-get install python-opencv
 
 #yes|sudo apt-get install python3-pip
 #python3 -m pip install jupyterhub
-jupyterhub --generate-config
+jupyter notebook --generate-config
+#jupyterhub --generate-config
 
 echo "install in sudo to get port 80"
